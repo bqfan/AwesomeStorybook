d9627e8f04194b62b10995457227bc287169cc67
Baiquan Fan <1252883+bqfan@users.noreply.github.com>
2025-04-25 19:11:41 +0300

    feat: add gtorybook backgrouds

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,21 @@ import "../global.css";
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#F7F9F2" },
+        { name: "dark", value: "#333" },
+        { name: "beige", value: "#F5ECE0" },
+        { name: "blue", value: "#5F99AE" },
+        { name: "teal", value: "#336D82" },
+        { name: "purple", value: "#693382" },
+      ],
+    },
+    initialGlobals: {
+      // 👇 Set the initial background color
+      backgrounds: { value: 'light' },
+    },
     // actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
